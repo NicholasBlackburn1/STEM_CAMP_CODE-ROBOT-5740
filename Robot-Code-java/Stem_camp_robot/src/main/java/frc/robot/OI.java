7866570cc2cc20f6094ec689fd015883a68c940c
nickblackburn1 <nickblackburn02@gmail.com>
2019-05-02 02:54:16 -0400

    added tallons

--- Robot-Code-java/Stem_camp_robot/src/main/java/frc/robot/OI.java
+++ Robot-Code-java/Stem_camp_robot/src/main/java/frc/robot/OI.java
@@ -7,10 +7,38 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
   
+  public static PowerDistributionPanel pdp = new PowerDistributionPanel(RobotMap.pdpCAN);
+
+  public static Joystick controller1 = new Joystick(RobotMap.controllerOnePort);
+
+  public static WPI_TalonSRX leftFrontDriveMotor = new WPI_TalonSRX(RobotMap.leftFrontDriveCAN);
+  public static WPI_TalonSRX leftCenterDriveMotor = new WPI_TalonSRX(RobotMap.leftCenterDriveCAN);
+  public static WPI_TalonSRX leftBackDriveMotor = new WPI_TalonSRX(RobotMap.leftBackDriveCAN);
+  public static WPI_TalonSRX rightFrontDriveMotor = new WPI_TalonSRX(RobotMap.rightFrontDriveCAN);
+  public static WPI_TalonSRX rightCenterDriveMotor = new WPI_TalonSRX(RobotMap.rightCenterDriveCAN);
+  public static WPI_TalonSRX rightBackDriveMotor = new WPI_TalonSRX(RobotMap.rightBackDriveCAN);
+
 }
--- Robot-Code-java/Stem_camp_robot/src/main/java/frc/robot/RobotMap.java
+++ Robot-Code-java/Stem_camp_robot/src/main/java/frc/robot/RobotMap.java
@@ -14,13 +14,14 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+  public static int pdpCAN = 6;
+  public static int controllerOnePort = 0;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+  public static int leftFrontDriveCAN = 15; 
+  public static int leftCenterDriveCAN = 11;
+  public static int leftBackDriveCAN = 12;
+  public static int rightFrontDriveCAN = 4;
+  public static int rightCenterDriveCAN = 13;
+  public static int rightBackDriveCAN = 14;
+  
 }
