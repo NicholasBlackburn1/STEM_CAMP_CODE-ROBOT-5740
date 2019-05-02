c7108a98f4b74278a7cfa6b19cc09895da0885bf
nickblackburn1 <nickblackburn02@gmail.com>
2019-05-02 03:45:18 -0400

    added / fixed robot.java

--- Robot-Code-java/Stem_camp_robot/src/main/java/frc/robot/Robot.java
+++ Robot-Code-java/Stem_camp_robot/src/main/java/frc/robot/Robot.java
@@ -12,8 +12,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.*;
 /**
  * The VM is configured to automatically run this class, and to call the
